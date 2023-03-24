191673876c67191e5f21b6b121f2ff2d1b7c65d8
mateoLorenzo1 <mateo.lorenzo@flydevs.com>
2023-03-24 20:20:25 -0300

    Removed yarn lock

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { AuthProvider } from "./context/AuthContext";
 import { ChampsProvider } from "./context/ChampsContext";
+import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 
 const App = () => {
   return (
     <AuthProvider>
       <ChampsProvider>
-        <LoginScreen />
+        {/* <LoginScreen /> */}
+        <HomeScreen />
       </ChampsProvider>
     </AuthProvider>
   );

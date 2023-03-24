e95297e0caf9a89d21d2a4eea11c7ffb13978a6a
Pedro <soypepe2308@gmail.com>
2023-03-24 20:07:22 -0300

    Added poppins font

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ChampDetailScreen from './screens/ChampDetailScreen';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <ChampDetailScreen />
   </React.StrictMode>
 );

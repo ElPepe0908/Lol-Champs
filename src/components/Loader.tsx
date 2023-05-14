35e5e51960bbd1c44740478c1994a09246c6e492
Pedro <soypepe2308@gmail.com>
2023-05-14 20:06:52 -0300

    Added a general Loader

---
+++ src/components/Loader.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Dot, LoaderContainer } from "./styles";
+
+export const Loader = () => {
+  return (
+    <LoaderContainer>
+      <Dot delay={0} />
+      <Dot delay={0.2} />
+      <Dot delay={0.4} />
+    </LoaderContainer>
+  );
+};
--- src/components/styles.tsx
+++ src/components/styles.tsx
@@ -1,7 +1,34 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+type DotAnimationProps = {
+  delay: number;
+};
 
 export const ChampionCard = styled("div")({
   width: "100%",
   height: "100%",
   background: "blue",
 });
+
+export const LoaderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+const dotPulse = keyframes`
+  0%, 80%, 100% { transform: scale(0); }
+  40% { transform: scale(1.0); }
+`;
+
+export const Dot = styled.div<DotAnimationProps>`
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  margin: 0 5px;
+  background-color: #5e5e77;
+  animation: ${dotPulse} 1s ease-in-out infinite;
+  animation-delay: ${(props) => props.delay}s;
+`;

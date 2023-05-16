da121e0a11fe23d599268190aa089e3ecf29f719
Pedro <soypepe2308@gmail.com>
2023-05-16 16:16:53 -0300

    Added a navigation between screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
 import { ChampsProvider } from "./context/ChampsContext";
 import { LoginScreen } from "./screens/LoginScreen/index";
 import { ChampDetailScreen } from "./screens/ChampDetailScreen/index";
 import { HomeScreen } from "./screens/HomeScreen/index";
+import {
+  BrowserRouter,
+  Navigate,
+  Outlet,
+  Route,
+  RouteProps,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
 
-const App = () => {
+export const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/login");
+  }, []);
+
   return (
     <AuthProvider>
       <ChampsProvider>
-        <ChampDetailScreen />
-        {/* <HomeScreen /> */}
-        {/* <LoginScreen /> */}
+        <Routes>
+          <Route path="/*" element={<HomeScreen />} />
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/champ-detail/:name" element={<ChampDetailScreen />} />
+        </Routes>
       </ChampsProvider>
     </AuthProvider>
   );
 };
 
-export default App;
+// const ProtectedRoutes = () => {
+//   return <Outlet />;
+// };
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { App } from "./App";
 import ChampDetailScreen from "./screens/ChampDetailScreen";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { Outlet, BrowserRouter as Router } from "react-router-dom";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
-    {/* <ChampDetailScreen /> */}
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </Router>
   </React.StrictMode>
 );
